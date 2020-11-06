3591418c4bd46aae9428e2ec5769b266f2551f05
didafer <dida.fernando@gmail.com>
2020-11-05 23:22:31 -0300

    add eight files

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,23 @@
+{ 
+    {
+         
+        {
+             {
+                  var sera = 'Sera???' 
+                  console.log(sera)
+                } 
+            } 
+        } 
+        console.log(sera)
+
+        function teste() {
+            var local = 173
+            console.log(local)
+        }
+    }
+
+    teste()
+    // console.log(local)
+    
+
+
